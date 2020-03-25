6a29585b1a5660317441ff9519d286240adfdb0f
georgegabor <georgegabor@rocketmail.com>
2020-03-25 14:11:45 +0100

    Form validation added

--- coordinates.js
+++ coordinates.js
@@ -1,3 +1,12 @@
+
+const form = document.getElementById('htmlform');
+
+form.addEventListener('submit', function (event) {
+  calc();
+  // Then we prevent the form from being sent by canceling the event
+  event.preventDefault();
+});
+
 function calc() {
   let longitude = document.getElementById('longitude');
   let latitude = document.getElementById('latitude');
@@ -11,5 +20,4 @@ function calc() {
   document.getElementById('wpoints').innerHTML = "Waypoints = " + waypoints.value;
   document.getElementById('fname').innerHTML = "Filename = " + filename.value.concat('.txt');
   document.getElementById('ps').innerHTML = "P.S.: Már csak a kalkuláció hiányzik :-) <br> Hajrá Dia !";
-
 }
