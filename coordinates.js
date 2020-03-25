3bbb94d52a0edd850c54b044ec9f9ff21489dfa8
georgegabor <georgegabor@rocketmail.com>
2020-03-25 13:19:10 +0100

    Updated styles

--- coordinates.js
+++ coordinates.js
@@ -3,11 +3,13 @@ function calc() {
   let latitude = document.getElementById('latitude');
   let distance = document.getElementById('distance');
   let waypoints = document.getElementById('waypoints');
+  let filename = document.getElementById('filename');
 
   document.getElementById('long').innerHTML = "Longitude = " + longitude.value;
   document.getElementById('lat').innerHTML = "Latitude = " + latitude.value;
   document.getElementById('dist').innerHTML = "Distance = " + distance.value;
   document.getElementById('wpoints').innerHTML = "Waypoints = " + waypoints.value;
+  document.getElementById('fname').innerHTML = "Filename = " + filename.value.concat('.txt');
   document.getElementById('ps').innerHTML = "P.S.: Már csak a kalkuláció hiányzik :-) <br> Hajrá Dia !";
 
 }
